d7fc680c1fe00f93de89cfb85d3f048e248f24af
Josef Paul L. Gelera <jgelera@up.edu.ph>
2024-04-27 19:41:07 +0800

    feat: handout shopping cart

---
+++ state_management/lib/model/Item.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: file_names
+class Item {
+  String name;
+  double price;
+  int quantity;
+
+  Item(this.name, this.price, this.quantity);
+}
---
+++ state_management/lib/screen/MyCart.dart
@@ -0,0 +1,94 @@
+// ignore_for_file: file_names
+import 'package:flutter/material.dart';
+import '../model/Item.dart';
+import 'package:provider/provider.dart';
+import '../provider/shoppingcart_provider.dart';
+
+class MyCart extends StatelessWidget {
+  const MyCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Cart"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          getItems(context),
+          computeCost(),
+          const Divider(
+            height: 4,
+            color: Colors.black,
+          ),
+          Flexible(
+              child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      context.read<ShoppingCart>().removeAll();
+                    },
+                    child: const Text("Reset"))
+              ],
+            ),
+          )),
+          TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/products");
+              },
+              child: const Text("Go back to Product Catalog"))
+        ],
+      ),
+    );
+  }
+
+  Widget getItems(BuildContext context) {
+    List<Item> products = context.watch<ShoppingCart>().cart;
+    String productname = "";
+
+    return products.isEmpty
+        ? const Text('No items yet!')
+        : Expanded(
+            child: Column(
+            children: [
+              Flexible(child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: const Icon(Icons.food_bank),
+                  title: Text(products[index].name),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      productname = products[index].name;
+                      context.read<ShoppingCart>().removeItem(productname);
+
+                      if (products.isNotEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text("$productname removed!"),
+                          duration:
+                              const Duration(seconds: 1, milliseconds: 100),
+                        ));
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text("Cart Empty!"),
+                          duration: Duration(seconds: 1, milliseconds: 100),
+                        ));
+                      }
+                    },
+                  ),
+                );
+              }))
+            ],
+          ));
+  }
+
+  Widget computeCost() {
+    return Consumer<ShoppingCart>(builder: (context, cart, child) {
+      return Text("Total: ${cart.cartTotal}");
+    });
+  }
+}
